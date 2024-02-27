6d80ac70634a73d3f5ba9762fb3b1d9461d5fb29
apenasjao <108680215+apenasjao@users.noreply.github.com>
2024-02-27 17:10:51 -0300

    povoamento dos equipamentos

--- povoamento.sql
+++ povoamento.sql
@@ -7,3 +7,85 @@ INSERT INTO TB_CLIENTE VALUES (
     TP_CLIENTE('12345678901234', 'Empresa XYZ', 'João Silva')
 );
 /
+
+-- INSERT EQUIPAMENTO
+INSERT INTO TB_EQUIPAMENTO VALUES (
+    TP_EQUIPAMENTO(1, 'Máquina de lavar', 'Eletrodoméstico', 'Sumsang')
+);
+/
+INSERT INTO TB_EQUIPAMENTO VALUES (
+    TP_EQUIPAMENTO(2, 'Martelo', 'Construção', 'Finto Pereira')
+);
+/
+INSERT INTO TB_EQUIPAMENTO VALUES (
+    TP_EQUIPAMENTO(3, 'Fita LED', 'Iluminação', 'Felips')
+);
+/
+INSERT INTO TB_EQUIPAMENTO VALUES (
+    TP_EQUIPAMENTO(4, 'Furadeira', 'Construção', 'Mikata')
+);
+/
+INSERT INTO TB_EQUIPAMENTO VALUES (
+    TP_EQUIPAMENTO(5, 'Ventilador', 'Eletrodoméstico', 'Orna')
+);
+/
+INSERT INTO TB_EQUIPAMENTO VALUES (
+    TP_EQUIPAMENTO(6, 'Mesa', 'Móvel', 'TikStok')
+);
+/
+INSERT INTO TB_EQUIPAMENTO VALUES (
+    TP_EQUIPAMENTO(7, 'Aspirador de Pó', 'Eletrodoméstico', 'Felips')
+);
+/
+INSERT INTO TB_EQUIPAMENTO VALUES (
+    TP_EQUIPAMENTO(8, 'Geladeira', 'Eletrodoméstico', 'Sumsang')
+);
+/
+INSERT INTO TB_EQUIPAMENTO VALUES (
+    TP_EQUIPAMENTO(9, 'Lâmpada', 'Iluminação', 'Felips')
+);
+/
+INSERT INTO TB_EQUIPAMENTO VALUES (
+    TP_EQUIPAMENTO(10, 'Micro-ondas', 'Eletrodoméstico', 'Sumsang')
+);
+/
+INSERT INTO TB_EQUIPAMENTO VALUES (
+    TP_EQUIPAMENTO(11, 'Batedeira', 'Eletrodoméstico', 'Felips')
+);
+/
+INSERT INTO TB_EQUIPAMENTO VALUES (
+    TP_EQUIPAMENTO(12, 'Capacete', 'Construção', 'Finto Pereira')
+);
+/
+INSERT INTO TB_EQUIPAMENTO VALUES (
+    TP_EQUIPAMENTO(13, 'Sofá', 'Móvel', 'TikStok')
+);
+/
+INSERT INTO TB_EQUIPAMENTO VALUES (
+    TP_EQUIPAMENTO(14, 'Motoserra', 'Construção', 'Mikata')
+);
+/
+INSERT INTO TB_EQUIPAMENTO VALUES (
+    TP_EQUIPAMENTO(15, 'Janela', 'Construção', 'Finto Pereira')
+);
+/
+INSERT INTO TB_EQUIPAMENTO VALUES (
+    TP_EQUIPAMENTO(16, 'Liquidificador', 'Eletrodoméstico', 'Orna')
+);
+/
+INSERT INTO TB_EQUIPAMENTO VALUES (
+    TP_EQUIPAMENTO(17, 'Luminária', 'Iluminação', 'Felips')
+);
+/
+INSERT INTO TB_EQUIPAMENTO VALUES (
+    TP_EQUIPAMENTO(18, 'Cafeteira', 'Eletrodoméstico', 'Tem Café')
+);
+/
+INSERT INTO TB_EQUIPAMENTO VALUES (
+    TP_EQUIPAMENTO(19, 'Ar Condicionado', 'Eletrodoméstico', 'Felips')
+);
+/
+INSERT INTO TB_EQUIPAMENTO VALUES (
+    TP_EQUIPAMENTO(20, 'Chuveiro', 'Construção', 'APAC')
+);
+/
